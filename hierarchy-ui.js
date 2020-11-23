a02f71127209652c9edd0b68ab372c981c506cb2
Egemen Berker Kızılcan <egemen@umomega.com>
2020-11-23 21:13:50 +0300

    Moved all components from main package

--- hierarchy-ui.js
+++ hierarchy-ui.js
@@ -1,3 +1,31 @@
+import ContentsChildren from '.js/views/contents/ContentsChildren'
+import ContentsCreate from '.js/views/contents/ContentsCreate'
+import ContentsEdit from '.js/views/contents/ContentsEdit'
+import ContentsIndex from '.js/views/contents/ContentsIndex'
+import ContentsSettings from '.js/views/contents/ContentsSettings'
+import ContentsStatistics from '.js/views/contents/ContentsStatistics'
+import ContentsTransform from '.js/views/contents/ContentsTransform'
+import ContentsTranslate from '.js/views/contents/ContentsTranslate'
+
+import ContentsDatable from './js/partials/ContentsDatable'
+import ContentsDropdown from './js/partials/ContentsDropdown'
+import ContentsHeader from './js/partials/ContentsHeader'
+import ContentsTags from './js/partials/ContentsTags'
+import ContentsTitle from './js/partials/ContentsTitle'
+import ContentsTree from './js/partials/ContentsTree'
+import ContentsEditHelper from './js/mixins/ContentsEditHelper'
+
+import ContentStored from './js/events/ContentStored'
+import ContentUpdated from './js/events/ContentUpdated'
+import ContentSettingsUpdated from './js/events/ContentSettingsUpdated'
+import ContentStateUpdated from './js/events/ContentStateUpdated'
+import ContentMoved from './js/events/ContentMoved'
+import ContentTransformed from './js/events/ContentTransformed'
+import ContentTranslated from './js/events/ContentTranslated'
+import ContentTranslationDestroyed from './js/events/ContentTranslationDestroyed'
+import ContentsDestroyedBulk from './js/events/ContentsDestroyedBulk'
+import ContentDestroyed from './js/events/ContentsDestroyed'
+
 import ContentFieldDestroyed from './js/events/ContentFieldDestroyed'
 import ContentFieldStored from './js/events/ContentFieldStored'
 import ContentFieldUpdated from './js/events/ContentFieldUpdated'
@@ -8,12 +36,16 @@ import ContentTypeUpdated from './js/events/ContentTypeUpdated'
 
 import ContentFieldsCreate from './js/views/contentfields/ContentFieldsCreate'
 import ContentFieldsEdit from './js/views/contentfields/ContentFieldsEdit'
+import ContentTypesContents from './js/views/contenttypes/ContentTypesContents'
 import ContentTypesCreate from './js/views/contenttypes/ContentTypesCreate'
 import ContentTypesEdit from './js/views/contenttypes/ContentTypesEdit'
 import ContentTypesFields from './js/views/contenttypes/ContentTypesFields'
 import ContentTypesIndex from './js/views/contenttypes/ContentTypesIndex'
 
 export {
+	ContentsChildren, ContentsCreate, ContentsEdit, ContentsIndex, ContentsSettings, ContentsStatistics, ContentsTransform, ContentsTranslate,
+	ContentsDatable, ContentsDropdown, ContentsHeader, ContentsTags, ContentsTitle, ContentsTree, ContentsEditHelper,
+	ContentStored, ContentUpdated, ContentSettingsUpdated, ContentStateUpdated, ContentMoved, ContentTransformed, ContentTranslated, ContentTranslationDestroyed, ContentsDestroyedBulk, ContentDestroyed,
 	ContentFieldDestroyed, ContentFieldStored, ContentFieldUpdated, ContentTypeDestroyed, ContentTypesDestroyedBulk, ContentTypeStored, ContentTypeUpdated,
-	ContentFieldsCreate, ContentFieldsEdit, ContentTypesCreate, ContentTypesEdit, ContentTypesFields, ContentTypesIndex
+	ContentFieldsCreate, ContentFieldsEdit, ContentTypesContents, ContentTypesCreate, ContentTypesEdit, ContentTypesFields, ContentTypesIndex
 }
